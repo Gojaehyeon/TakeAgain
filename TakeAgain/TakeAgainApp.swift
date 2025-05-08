4f5f5831c720ad359e959280e23ed73794279d9c
Gojaehyeon <gojaehyeonstudio@gmail.com>
2025-05-08 19:16:12 +0900

    Initial Commit

---
+++ TakeAgain/TakeAgainApp.swift
@@ -0,0 +1,32 @@
+//
+//  TakeAgainApp.swift
+//  TakeAgain
+//
+//  Created by 고재현 on 5/8/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TakeAgainApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TakeAgainTests/TakeAgainTests.swift
@@ -0,0 +1,17 @@
+//
+//  TakeAgainTests.swift
+//  TakeAgainTests
+//
+//  Created by 고재현 on 5/8/25.
+//
+
+import Testing
+@testable import TakeAgain
+
+struct TakeAgainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
